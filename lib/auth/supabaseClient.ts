501398f5a97fe7ec48a6c4a3b6a8ad2a1a91c10e
Albert Zhao <zhaoq@kean.edu>
2024-09-08 13:41:02 +0800

    Revert "feat: Add Supabase authentication for server and client"
    This reverts commit 2f828a27c5b9e75920f36a907e9c2316bdc16ef1.

--- lib/auth/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: 'env.local' })
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
