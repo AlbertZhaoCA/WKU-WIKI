aa4e9792822d5a4e80f6bde2aa9b0cc288f691ca
Albert Zhao <zhaoq@kean.edu>
2024-09-08 13:41:02 +0800

    Revert "routine update"
    This reverts commit 9c0e494d547c61793030f3ca19386967eab8479c.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,5 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
 import ComingSoon from '@/components/ComingSoon'
-import { supabase } from 'lib/auth/supabaseClient'
 
-export default function Page() {
-  const [loading, setLoading] = useState(true)
-  const router = useRouter()
-
-  useEffect(() => {
-    const checkUser = async () => {
-      const user = await supabase.auth.getUser()
-      console.log(user)
-      if (!user) {
-        router.push('/login')
-      } else {
-        setLoading(false)
-      }
-    }
-
-    checkUser()
-  }, [router])
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
+export default function page() {
   return <ComingSoon />
 }
